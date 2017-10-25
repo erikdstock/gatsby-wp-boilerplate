b9123ebd25fd3879920a36e78418dd430685a73b
Erik Stockmeier <erik.d.stock@gmail.com>
2017-10-25 00:02:00 -0400

    add dotenv for wp integration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -16,9 +17,9 @@ module.exports = {
         * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
         * Example : 'gatsbyjsexamplewordpress.wordpress.com' or 'www.example-site.com'
         */
-        baseUrl: 'snyctest.wordpress.com',
+        baseUrl: process.env.WORDPRESS_BASE_URL,
         // The protocol. This can be http or https.
-        protocol: 'http',
+        protocol: 'https',
         // Indicates whether the site is hosted on wordpress.com.
         // If false, then the asumption is made that the site is self hosted.
         // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
@@ -31,21 +32,21 @@ module.exports = {
         auth: {
           // If auth.user and auth.pass are filled, then the source plugin will be allowed
           // to access endpoints that are protected with .htaccess.
-          htaccess_user: 'your-htaccess-username',
-          htaccess_pass: 'your-htaccess-password',
-          htaccess_sendImmediately: false,
+          // htaccess_user: 'your-htaccess-username',
+          // htaccess_pass: 'your-htaccess-password',
+          // htaccess_sendImmediately: false,
 
           // If hostingWPCOM is true then you will need to communicate with wordpress.com API
           // in order to do that you need to create an app (of type Web) at https://developer.wordpress.com/apps/
           // then add your clientId, clientSecret, username, and password here
-          wpcom_app_clientSecret: 'NMPnXYFtj2gKas7V1kZyMxr7oLry9V5ZxIyBQGu2txjVHg0GhFz6RYcKopkHICYg',
-          wpcom_app_clientId: '54793',
-          wpcom_user: 'gatsbyjswpexample@gmail.com',
-          wpcom_pass: 'very-secured-password',
+          wpcom_app_clientSecret: process.env.WPCOM_CLIENT_SECRET,
+          wpcom_app_clientId: process.env.WPCOM_CLIENT_ID,
+          // wpcom_user: process.env.WPCOM_USER,'gatsbyjswpexample@gmail.com',
+          // wpcom_pass: process.env.WPCOM_PASSWORD,'very-secured-password',
         },
         // Set verboseOutput to true to display a verbose output on `npm run develop` or `npm run build`
         // It can help you debug specific API Endpoints problems
-        verboseOutput: false,
+        verboseOutput: true,
       },
     }
   ],
